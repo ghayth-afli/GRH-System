08b51d37da812ad628a8cf6664d93112ed7e1ca5
Mohamed Ghayth Afli <mohamed.ghayth12@gmail.com>
2025-03-01 05:41:18 +0100

    added apiGTW routes

--- backend/auth/src/main/java/com/otbs/auth/controller/PasswordResetController.java
+++ backend/auth/src/main/java/com/otbs/auth/controller/PasswordResetController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/auth")
+@RequestMapping("/api/v1/auth")
 @CrossOrigin(origins = "*", maxAge = 3600)
 public class PasswordResetController {
 
